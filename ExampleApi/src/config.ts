82e3cbf0e2dfc8f230fa0663cb6cf417a827faf2
Erin O'Connor <erino@udel.edu>
2020-05-03 13:03:39 -0400

    reorganized folder structure

--- ExampleApi/src/config.ts
+++
@@ -1,7 +0,0 @@
-//configuration information 
-export const Config={
-    serverport: process.env.PORT || 3000,
-    secret: process.env.SECRET || "some-secret-goes-here",
-    tokenLife: 1800,
-    url: process.env.MONGOURL || "mongodb://localhost:27017/"    
-};
