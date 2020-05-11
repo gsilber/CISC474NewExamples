d3561552daa2738826d5710f44172680ad402a6a
gsilber <silber@udel.edu>
2020-05-11 14:50:34 -0400

    different api

--- ExampleApi/src/proxy/proxyController.ts
+++ ExampleApi/src/proxy/proxyController.ts
@@ -5,11 +5,13 @@ export class ProxyController {
     getFromOtherApi(req: express.Request, res: express.Response){
             const options={
                 method: 'GET',
-                url: 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/'+req.params.searchString,
+                url: 'https://imdb8.p.rapidapi.com/title/auto-complete',
+                qs: {q: req.params.searchString},
                 headers: {
-                  'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com',
-                  'x-rapidapi-key': 'a2fe662994msh367be85e2f1be3cp11f4dajsn15586788801c' //change to your key
-                }   
+                  'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+                  'x-rapidapi-key': 'a2fe662994msh367be85e2f1be3cp11f4dajsn15586788801c',
+                  useQueryString: true
+                            }   
             };
             request(options,function(error,response,result){
                 if (error) return res.status(500).send(error);
