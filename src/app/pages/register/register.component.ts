b5c0568c918c1ed5fa5e1891f75e488fdf3d8e4b
Dan Paddock <danpaddock@dans-air.lan>
2020-05-12 17:07:49 -0400

    empty graph under register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvasjs.min';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit() {
+	let dataPoints = [];
+	let y = 0;		
+	for ( var i = 0; i < 10000; i++ ) {		  
+		y += Math.round(5 + Math.random() * (-5 - 5));	
+		dataPoints.push({ y: y});
+	}
+	let chart = new CanvasJS.Chart("chartContainer", {
+		zoomEnabled: true,
+		animationEnabled: true,
+		exportEnabled: true,
+		title: {
+			text: "Performance Demo - 10000 DataPoints"
+		},
+		subtitles:[{
+			text: "Try Zooming and Panning"
+		}],
+		data: [
+		{
+			type: "line",                
+			dataPoints: dataPoints
+		}]
+	});
+		
+	chart.render();
+    }
 }
