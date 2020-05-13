2f1a88654ee6b8e0f13daf85ca1c7631ccca7fc8
Charlie <charlie.hannum@gmail.com>
2020-05-13 13:52:22 -0400

    removed unneccessary files and pages from site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { AddprojectComponent } from './pages/addproject/addproject.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TokenInterceptorService } from './services/token-interceptor.service';
@@ -18,9 +15,6 @@ import { D3MapComponent } from './d3-map/d3-map.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    AddprojectComponent,
     D3MapComponent
   ],
   imports: [
