3d26894d0ec6f964c336a5f1981413090a929547
Dan Paddock <danpaddock@dans-air.lan>
2020-05-13 14:11:27 -0400

    fixed page loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { GraphComponent } from './pages/graph/graph.component';
 import { AddprojectComponent } from './pages/addproject/addproject.component';
+import { D3MapComponent } from './d3-map/d3-map.component'
 
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path:'home',component: HomeComponent},
   {path:'login',component: LoginComponent},
   {path:'graph',component: GraphComponent},
-  {path:'addproject',component: AddprojectComponent}
+  {path:'addproject',component: AddprojectComponent},
+  {path:'d3-map',component: D3MapComponent}
 ];
 
 @NgModule({
